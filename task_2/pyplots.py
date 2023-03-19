9f58e37888d6644ff0f254bb2193d42139d1934a
Александр <62289323+busyhedg03@users.noreply.github.com>
2023-03-20 05:50:27 +0700

    Add files via upload

--- task_2/pyplots.py
+++ task_2/pyplots.py
@@ -3,34 +3,36 @@ import matplotlib.pyplot as plt
 import math
 import matplotlib.colors as mcolors
 import numpy as np
-# cat_par = [f"{i}*{i}" for i in [128, 256, 512, 1024]]
 
-# g1 = [0.5, 0.67, 100, 0] 
-# g2 = [1.1, 6.5, 45, 340]
+cpu_one = [0.1, 1.8, 25, 0] 
+cpu_multi = [0.5, 3.5, 20, 145]
+gpu = [0.3, 0.5, 1.5, 16.7]
+gpu_opt = [0.95, 0.73, 0.7, 0.5, 0.25, 0.23]
 
-# width = 0.3
+cat_par = [f"{i}*{i}" for i in [128, 256, 512, 1024]]
+width = 0.3
 
-# x = np.arange(len(cat_par))
+x = np.arange(len(cat_par))
 
-# fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 
-# rects1 = ax.bar(x - width/2, g1, width, label='Onecore')
-# rects2 = ax.bar(x + width/2, g2, width, label='Multicore')
+rects1 = ax.bar(x - width/2, cpu_one, width, label='Onecore')
+rects2 = ax.bar(x + width/2, cpu_multi, width, label='Multicore')
 
-# ax.set_title('Onecore and multicore time, s')
-# ax.set_xticks(x)
-# ax.set_xticklabels(cat_par)
-# ax.set_axisbelow(True)
-# ax.legend()
-# fig.set_figheight(7)
-# plt.grid(axis = 'y')
-# fig.savefig('cpu.png')
+ax.set_title('Onecore and multicore time, s')
+ax.set_xticks(x)
+ax.set_xticklabels(cat_par)
+ax.set_axisbelow(True)
+ax.legend()
+fig.set_figheight(7)
+plt.grid(axis = 'y')
+fig.savefig('cpu.png')
 
 
 
-cat_par = [f"{i+1}" for i in range(5)]
+cat_par = [f"{i+1}" for i in range(6)]
 
-g1 = [0.95, 0.73, 0.7, 0.5, 0.25]
+
 
 width = 0.3
 
@@ -38,7 +40,7 @@ x = np.arange(len(cat_par))
 
 fig, ax = plt.subplots()
 
-rects1 = ax.bar(x, g1, width, label='GPU time, s')
+rects1 = ax.bar(x, gpu_opt, width, label='GPU time, s')
 
 ax.set_title('Этапы оптимизации')
 ax.set_xticks(x)
@@ -53,25 +55,22 @@ fig.savefig('opt.png')
 
 
 
-# cat_par = [f"{i}*{i}" for i in [128, 256, 512, 1024]]
+cat_par = [f"{i}*{i}" for i in [128, 256, 512, 1024]]
 
-# g1 = [0.5, 0.67, 100, 0] 
-# g2 = [1.1, 6.5, 45, 340]
-# g3 = [0.5, 0.8, 3.1, 34]
-# width = 0.3
+width = 0.3
 
-# x = np.arange(len(cat_par))
+x = np.arange(len(cat_par))
 
-# fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 
-# rects1 = ax.bar(x - width, g1, width, label='Onecore')
-# rects2 = ax.bar(x, g2, width, label='Multicore')
-# rects3 = ax.bar(x + width, g3, width, label='GPU')
-# ax.set_title('Onecore and multicore time, s')
-# ax.set_xticks(x)
-# ax.set_xticklabels(cat_par)
-# ax.set_axisbelow(True)
-# ax.legend()
-# fig.set_figheight(7)
-# plt.grid(axis = 'y')
-# fig.savefig('gpu.png')
+rects1 = ax.bar(x - width, cpu_one, width, label='Onecore')
+rects2 = ax.bar(x, cpu_multi, width, label='Multicore')
+rects3 = ax.bar(x + width, gpu, width, label='GPU')
+ax.set_title('Onecore and multicore time, s')
+ax.set_xticks(x)
+ax.set_xticklabels(cat_par)
+ax.set_axisbelow(True)
+ax.legend()
+fig.set_figheight(7)
+plt.grid(axis = 'y')
+fig.savefig('gpu.png')
