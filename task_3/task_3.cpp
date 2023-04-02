804bcce98c25c4f91d222dd59434765b5933c260
Александр <62289323+busyhedg03@users.noreply.github.com>
2023-04-03 00:47:45 +0700

    Adif = acc_malloc

--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -44,73 +44,59 @@ void print_array(T *A, int size)
     {
         for (int j = 0; j < size; ++j)
         {
-            // Значение с GPU
             #pragma acc kernels present(A)
             printf("%.2f\t", A[IDX2C(i, j, size)]);
         }
-        std::cout << std::endl;
+        printf("\n");
     }
-    std::cout << std::endl;
+    printf("\n");
 }
 
 // Инициализация матрицы, чтобы подготовить ее к основному алгоритму
 void initialize_array(T *A, int size)
 {
-    // Инициализируется матрица на GPU
-    #pragma acc parallel present(A)
+    // Заполнение углов матрицы значениями
+    A[IDX2C(0, 0, size)] = 10.0;
+    A[IDX2C(0, size - 1, size)] = 20.0;
+    A[IDX2C(size - 1, 0, size)] = 20.0;
+    A[IDX2C(size - 1, size - 1, size)] = 30.0;
+
+    // Заполнение периметра матрицы
+    T step = 10.0 / (size - 1);
+
+    for (int i = 1; i < size - 1; ++i)
     {
-        // Заполнение углов матрицы значениями
-        A[IDX2C(0, 0, size)] = 10.0;
-        A[IDX2C(0, size - 1, size)] = 20.0;
-        A[IDX2C(size - 1, 0, size)] = 20.0;
-        A[IDX2C(size - 1, size - 1, size)] = 30.0;
-
-        // Заполнение периметра матрицы
-        T step = 10.0 / (size - 1);
-        #pragma acc loop independent
-        for (int i = 1; i < size - 1; ++i)
-        {
-            T addend = step * i;
-            A[IDX2C(0, i, size)] = A[IDX2C(0, 0, size)] + addend;                 // horizontal
-            A[IDX2C(size - 1, i, size)] = A[IDX2C(size - 1, 0, size)] + addend;   // horizontal
-            A[IDX2C(i, 0, size)] = A[IDX2C(0, 0, size)] + addend;                 // vertical
-            A[IDX2C(i, size - 1, size)] = A[IDX2C(0, size - 1, size)] + addend;   // vertical
-        }
-
-        // Заполнение 20-ю, чтобы сократить количество операций в основном алгоритме
-        #pragma acc loop independent collapse(2)
-        for (int i = 1; i < size - 1; ++i)
-            for (int j = 1; j < size - 1; ++j)
-                A[IDX2C(i, j, size)] = 20.0;
+        T addend = step * i;
+        A[IDX2C(0, i, size)] = A[IDX2C(0, 0, size)] + addend;                 // horizontal
+        A[IDX2C(size - 1, i, size)] = A[IDX2C(size - 1, 0, size)] + addend;   // horizontal
+        A[IDX2C(i, 0, size)] = A[IDX2C(0, 0, size)] + addend;                 // vertical
+        A[IDX2C(i, size - 1, size)] = A[IDX2C(0, size - 1, size)] + addend;   // vertical
     }
+
+    //Заполнение 20-ю, чтобы сократить количество операций в основном алгоритме
+    for (int i = 1; i < size - 1; ++i)
+        for (int j = 1; j < size - 1; ++j)
+            A[IDX2C(i, j, size)] = 20.0;
 }
 
 // Основной алгоритм
 void calculate(int net_size = 128, int iter_max = 1e6, T accuracy = 1e-6, bool res = false)
 {
-    acc_set_device_num(2,acc_device_default); 
+    //acc_set_device_num(2,acc_device_default); 
     #ifdef _NVTX
         nvtxRangePush("Initialization");
     #endif
-    // Создаем указатель на структуру, содержащую контекст
-    cublasHandle_t handle;
-    // Инициализация контекста
-    CUBLAS_CHECK(cublasCreate(&handle));
     // Размер вектора - размер сетки в квадрате
     int vec_size = net_size * net_size;
     // Флаг обновления ошибки на хосте для обработки условием цикла
     bool update_flag = true;
     // Создание 2-х матриц (векторов), одна будет считаться на основе другой. И еще одна для разности
     T *Anew = new T [vec_size],
-      *A = new T [vec_size],
-      *Adif = new T [vec_size];
-
-    #pragma acc enter data create(A[:vec_size], Anew[:vec_size], Adif[:vec_size])
-
+      *A = new T [vec_size];
+    T *Adif = (T*)acc_malloc(sizeof(T) * vec_size);
     // Инициализация матриц
     initialize_array(A, net_size);
     initialize_array(Anew, net_size);
-    
     #ifdef _NVTX
         nvtxRangePop();
     #endif
@@ -129,14 +115,23 @@ void calculate(int net_size = 128, int iter_max = 1e6, T accuracy = 1e-6, bool r
     #ifdef _NVTX
         nvtxRangePush("Main Algorithm");
     #endif
-    #pragma acc enter data copyin(error)
+    // Создаем указатель на структуру, содержащую контекст
+    cublasHandle_t handle;
+    // Инициализация контекста
+    CUBLAS_CHECK(cublasCreate(&handle));
+    #pragma acc enter data copyin(A[:vec_size], Anew[:vec_size], error)
+    if (res)
+    {
+        std::cout << "--Borders--" << std::endl;
+        print_array(A, net_size);
+    }
     for (iter = 0; iter < iter_max; ++iter)
     {
         // Сокращение количества обращений к CPU. Больше сетка - реже стоит сверять значения.
         update_flag = !(iter % net_size);
 
         // Подсчет матрицы по среднему соседей в другой матрице
-        #pragma acc kernels loop independent collapse(2) present(A, Anew) async(1)
+        #pragma acc kernels loop independent collapse(2) present(A, Anew) vector_length(128) async(1)
         for (int i = 1; i < net_size - 1; i++)
             for (int j = 1; j < net_size - 1; j++)
                 Anew[IDX2C(i, j, net_size)] = (A[IDX2C(i + 1, j, net_size)] + A[IDX2C(i - 1, j, net_size)]
@@ -149,9 +144,9 @@ void calculate(int net_size = 128, int iter_max = 1e6, T accuracy = 1e-6, bool r
             // зануление ошибки на GPU
             #pragma acc kernels present(error)
             error = 0;
-            #pragma acc data present(A, Anew, Adif) wait(1)
+            #pragma acc data present(A, Anew) deviceptr(Adif)
             {
-                #pragma acc host_data use_device(A, Anew, Adif)
+                #pragma acc host_data use_device(A, Anew)
                 {
                     // Adif = Anew
                     CUBLAS_CHECK(cublascopy(handle, vec_size, Anew, inc, Adif, inc));
@@ -165,7 +160,7 @@ void calculate(int net_size = 128, int iter_max = 1e6, T accuracy = 1e-6, bool r
                 }
             }
             // Обновление ошибки на хосте
-            #pragma acc update host(error)
+            #pragma acc update host(error) wait(1)
             // Если ошибка не превышает точность, прекратить выполнение цикла
             if (error <= accuracy)
                 break;
@@ -173,20 +168,23 @@ void calculate(int net_size = 128, int iter_max = 1e6, T accuracy = 1e-6, bool r
     }
     // Синхронизация
     #pragma acc wait(1)
+    acc_free(Adif);
     
     #ifdef _NVTX
         nvtxRangePop();
     #endif
     std::cout.precision(2);
     if (res)
-        print_array(A, net_size);
+    {
+        std::cout << "--Result--" << std::endl;
+        print_array(Anew, net_size);
+    }
     std::cout << "iter=" << iter << ",\terror=" << error << std::endl;
 
     cublasDestroy(handle);
-    #pragma acc exit data delete (A[:vec_size], Anew[:vec_size], Adif[:vec_size], error)
+    #pragma acc exit data delete (A[:vec_size], Anew[:vec_size], error)
     delete[] A;
     delete[] Anew;
-    delete[] Adif;
 }
 
 int main(int argc, char *argv[])
